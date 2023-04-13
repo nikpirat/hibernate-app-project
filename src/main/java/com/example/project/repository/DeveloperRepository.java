802c7ec32ff1f4cba40041dcf5b3e82faded6a35
nikpirat <javadeveloper2077@gmail.com>
2023-04-13 11:34:10 +0300

    #commit 2: added repository interface layer

---
+++ src/main/java/com/example/project/repository/DeveloperRepository.java
@@ -0,0 +1,6 @@
+package com.example.project.repository;
+
+import com.example.project.model.Developer;
+
+public interface DeveloperRepository extends GenericRepository<Developer, Long> {
+}
---
+++ src/main/java/com/example/project/repository/SpecialtyRepository.java
@@ -0,0 +1,6 @@
+package com.example.project.repository;
+
+import com.example.project.model.Specialty;
+
+public interface SpecialtyRepository extends GenericRepository<Specialty, Long> {
+}
